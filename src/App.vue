0cdf695b5ce0ffda535813ce6bd491af4b5b8881
karenolea <karen.oleatoledo@gmail.com>
2021-08-01 22:14:20 -0400

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <p>
+        <label for="">Tarea</label>
+        <input v-model="respuesta" placeholder="ingresa nueva tarea" />
+        <button @click.prevent="btn">Añade</button>
+      </p>
+    </form>
+
+    <hello-world :list="this.tareas" @eliminar="eliminar"> </hello-world>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { HelloWorld },
+  
+  data() {
+    return {
+      respuesta: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    btn() {
+      this.tareas.push(this.respuesta);
+      this.respuesta = "";
+    },
+    eliminar(index){
+      this.tareas.splice(index,1)
+    }
+  },
+};
 </script>
 
 <style>
@@ -24,5 +45,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding-right: 0.5rem;
 }
 </style>
